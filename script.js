45de4cef7ca6bb8a41d7f82ea1d4a6718d369b83
Pignataro67 <Pignataro67@gmail.com>
2022-02-09 04:49:55 -0800

    JS

---
+++ script.js
@@ -0,0 +1,7 @@
+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
+
+const testimonialsContainer = document.querySelector('.testimonials-container')
+const testimonial = document.querySelector('.testimonial')
+const userImage = document.querySelector('.user-image')
+const username = document.querySelector('.username')
+const role = document.querySelector('.role')
