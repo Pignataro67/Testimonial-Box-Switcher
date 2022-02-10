90eae1c6bdaed5f072164c26254c165db623d3ba
Pignataro67 <Pignataro67@gmail.com>
2022-02-10 05:05:11 -0800

    JS

--- script.js
+++ script.js
@@ -22,4 +22,18 @@ const testimonials = [
     text:
       'This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
   },
+  {
+    name: 'Iida Niskanen',
+    position: 'Data Entry',
+    photo: 'https://randomuser.me/api/portraits/women/68.jpg',
+    text:
+      "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
+  },
+  {
+    name: 'Renee Sims',
+    position: 'Receptionist',
+    photo: 'https://randomuser.me/api/portraits/women/65.jpg',
+    text:
+      "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
+  },
 ]
